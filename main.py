72d33f6b7e683a2c7be080afaae51e5f824d2a5b
Fathima sameeha23f2000299@ds.study.iitm.ac.in <23f2000299@ds.study.iitm.ac.in>
2025-02-11 20:08:32 +0530

    Created database schema for users, subjects, quizzes, and scores

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 from flask import Flask, render_template
 from controllers.database import db
-from controllers.config import Config  # Import Config
-from controllers.models import User, Score, Subject, Chapter, Question, Quiz
+from controllers.config import Config
+from controllers.models import *
 
-app = Flask(__name__, template_folder="templates", static_folder="static")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite3"
+app = Flask(__name__)
+app.config.from_object(Config)
 
 db.init_app(app)
 
 with app.app_context():
-    db.create_all()  # Creates tables in SQLite
+    db.create_all()
 
 @app.route('/')
 def hello():
-    return render_template("home.html")
-
-@app.route('/about')
-def about():
-    return "About"
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
